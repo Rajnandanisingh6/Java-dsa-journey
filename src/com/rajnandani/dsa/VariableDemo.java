96d7b20fd86b10abf599e2f71d89720e297d03a7
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2026-06-13 18:42:42 +0530

    Variables program added

---
+++ src/com/rajnandani/dsa/VariableDemo.java
@@ -0,0 +1,22 @@
+package com.rajnandani.dsa;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		//create a String variable name and print it.
+		 String name = "Rajnandani Singh";
+		 System.out.println("My name is "+name);
+		
+		// create an integer variable age and print it.
+		int age = 18;
+		System.out.println("My age is " + age);
+		
+		//store your college name in a variable.
+		String college_name = "GLA University";
+		System.out.println("My college name is "+college_name);
+		
+		
+
+	}
+
+}
